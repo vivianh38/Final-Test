bd587118358dcf906ff5dbd40feefb52f60778d6
Ying-Chau Huang <yingchauhuang@gmail.com>
2020-08-16 10:32:49 +0800

    commit 1
    Signed-off-by: Ying-Chau Huang <yingchauhuang@gmail.com>

--- app/src/main/java/com/example/homework2/MainActivity.java
+++ app/src/main/java/com/example/homework2/MainActivity.java
@@ -25,17 +25,12 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.QRCodeDetector;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     //Vivian Huang
     ImageView imageView;
+    ImageView imageView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,41 +41,72 @@ public class MainActivity extends AppCompatActivity {
         else{
             Log.d("test", "OpenCV is fine");
         }
+
         imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
         Button btn = findViewById(R.id.button);
+
         Log.d("viv","alright so far");
+
+
         btn.setOnClickListener(new View.OnClickListener() {
             int count;
             @Override
             public void onClick(View view) {
+
                 if(count != 1){
                     Bitmap bm = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                    Bitmap bmm = ((BitmapDrawable) imageView2.getDrawable()).getBitmap();
                     String output = decodeQRCode(bm);
+
+
                     //textView.setText(output);
-                    Mat img = new Mat(bm.getWidth(), bm.getHeight(), CvType.CV_8UC4);
-                    Utils.bitmapToMat(bm, img);
+
+                    Mat img2 = new Mat(bmm.getWidth(), bmm.getHeight(), CvType.CV_8UC4);
+                    Utils.bitmapToMat(bmm, img2);
+
+                    Log.d("viv", " "+ output);
+
                     Scalar lineColor = new Scalar(255, 0, 0, 255);
                     int lineWidth = 8;
-                    String [] lines = output.split(";");
+
+
+                    String [] lines = output.split(" ");
+
+                    Point[] points = {new Point(), new Point(), new Point(), new Point(), new Point(), new Point()};
                     int totalSets = lines.length;
+
                     for(int i = 0; i < totalSets; i++) {
-                        String [] coor = lines[i].split(" ");
-                        int totalCoors = coor.length;
-                        Point[] points = {new Point(), new Point()};
-                        for(int k = 0; k < totalCoors; k++){
-                            String [] xny = coor[k].split(",");
-                            points[k] = new Point(Integer.parseInt(xny[0]),Integer.parseInt(xny[1]));
-                        }
-                        Imgproc.line(img, points[0], points[1], lineColor, lineWidth);
+                        String [] coords = lines[i].split(",");
+
+                        points[i] = new Point(Integer.parseInt(coords[0])/2,Integer.parseInt(coords[1])/2);
+
+                        Log.d("viv", " "+points[i]);
+
+                        //Imgproc.line(img2, points[0], points[1], lineColor, lineWidth);
+                        Log.d("viv","alright so far!!");
                     }
-                    Bitmap bitmap = Bitmap.createBitmap(img.width(), img.height(), Bitmap.Config.ARGB_8888);
-                    Utils.matToBitmap(img, bitmap);
-                    imageView.setImageBitmap(bitmap);
+                    Imgproc.line(img2, points[0], points[1], lineColor, lineWidth);
+                    Imgproc.line(img2, points[1], points[2], lineColor, lineWidth);
+                    Imgproc.line(img2, points[2], points[3], lineColor, lineWidth);
+                    Imgproc.line(img2, points[3], points[4], lineColor, lineWidth);
+                    Imgproc.line(img2, points[4], points[5], lineColor, lineWidth);
+
+
+                    imageView.setImageBitmap(bm);
+
+                    Bitmap bitmap2 = Bitmap.createBitmap(img2.width(), img2.height(), Bitmap.Config.ARGB_8888);
+                    Utils.matToBitmap(img2, bitmap2);
+                    imageView2.setImageBitmap(bitmap2);
+
                 }
                 count++;
             }
         });
     }
+
+
+
     String decodeQRCode(Bitmap bitmap){
         Mat mat = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC4);
         Utils.bitmapToMat(bitmap,mat);
